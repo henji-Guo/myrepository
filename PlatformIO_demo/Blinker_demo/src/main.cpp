1b7354ca49d2a5afefaef964bbe3858434357c59
GHJ <1012323780@qq.com>
2020-07-24 13:33:05 +0800

    update

--- PlatformIO_demo/Blinker_demo/src/main.cpp
+++ PlatformIO_demo/Blinker_demo/src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+#define BLINKER_WIFI
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Blinker.h>
+
+char auth[] = "47c18edb43ee";
+char ssid[] = "360WiFi-3F8418";
+char pswd[] = "wzlwggw001001";
+
+// 新建组件对象
+BlinkerButton Button1("btn-abc");
+BlinkerNumber Number1("num-abc");
+
+int counter = 0;
+
+// 按下按键即会执行该函数
+void button1_callback(const String & state)
+{
+    BLINKER_LOG("get button state: ", state);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+// 如果未绑定的组件被触发，则会执行其中内容
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+    counter++;
+    Number1.print(counter);
+}
+
+void setup()
+{
+    // 初始化串口
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+    
+    // 初始化有LED的IO
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    // 初始化blinker
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+
+    Button1.attach(button1_callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    Blinker.run();
 }
